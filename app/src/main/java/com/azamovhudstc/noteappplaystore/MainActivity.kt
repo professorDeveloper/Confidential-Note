f3eed0f1af53be04b5a8a9465aecd5cde222a5bd
professorDeveloper <azamovhud007@outlook.com>
2023-02-04 18:17:06 +0500

    Confidential Note

--- app/src/main/java/com/azamovhudstc/noteappplaystore/MainActivity.kt
+++ app/src/main/java/com/azamovhudstc/noteappplaystore/MainActivity.kt
@@ -12,7 +12,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
